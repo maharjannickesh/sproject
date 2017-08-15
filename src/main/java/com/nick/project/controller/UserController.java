4dd1c236da4c8733032be3b1732d57d16aaa7942
Maharjan <nickesh.maharjan@gmail.com>
2017-08-15 15:36:46 -0400

    Post user

--- src/main/java/com/nick/project/controller/UserController.java
+++ src/main/java/com/nick/project/controller/UserController.java
@@ -2,39 +2,55 @@ package com.nick.project.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.nick.project.entity.User;
 import com.nick.project.service.UserService;
+
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    @ModelAttribute("user")
-    private User construct() {
-        return new User();
-    }
+	@ModelAttribute("user")
+	private User construct() {
+		return new User();
+	}
 
-    @RequestMapping(value = "/all", method = RequestMethod.GET)
-    public List<User> allUser() {
-        System.out.println("Inside All User");
-        return null;
-        // return userService.saveUser();
-    }
+	@RequestMapping(value = "/all", method = RequestMethod.GET)
+	public List<User> allUser() {
+		System.out.println("Inside All User");
+		// return null;
+		// userService.saveUser();
+		return userService.getAllUser();
+	}
 
-    @RequestMapping(value = "/add")
-    public String addUser() {
-        System.out.println("Inside ADD user");
-        userService.saveUser();
-        return "Done";
-    }
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String addUser(@Valid @RequestBody User user, BindingResult result) {
+
+		System.out.println(user.getFirstName());
+		System.out.println("inside Save");
+		userService.addNewUser(user);
+
+		if (result.hasErrors()) {
+			return "0";
+		}
+		return "1";
+
+		/*
+		 * System.out.println("Inside ADD user"); userService.saveUser(); return
+		 * "Done";
+		 */
+	}
 
 }
--- src/main/java/com/nick/project/service/UserService.java
+++ src/main/java/com/nick/project/service/UserService.java
@@ -1,37 +1,66 @@
 package com.nick.project.service;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.transaction.Transactional;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.nick.project.entity.User;
 import com.nick.project.repository.UserRepository;
 
 @Service
+
 public class UserService {
 
-    @Autowired
-    UserRepository userRepository;
+	@Autowired
+	UserRepository userRepository;
 
-    Date date = new Date(new java.util.Date().getTime());
+	Date date = new Date(new java.util.Date().getTime());
 
-    User newUser = new User();
+	User newUser = new User();
 
-    @Transactional
-    public void saveUser() {
-        System.out.println("Inside Save User");
-        newUser.setFirstName("New");
-        newUser.setLastName("Maharjan");
-        newUser.setEmail("abc@abc.com");
-        newUser.setPassword("123456789");
-        newUser.setEnabled(true);
-        newUser.setAddedBy("Nikesh");
-        newUser.setAddedTime(date.toString());
-        newUser.setUpdatedBy("NikeshUPD");
-        newUser.setUpdatedTime(date.toString());
+	List<User> users = new ArrayList<>();
 
-        userRepository.save(newUser);
+	public void saveUser() {
 
-    }
+		System.out.println("Inside Save User");
+		newUser.setFirstName("New");
+		newUser.setLastName("Maharjan");
+		newUser.setEmail("abc@abc.com");
+		newUser.setPassword("123456789");
+		newUser.setEnabled(true);
+		newUser.setAddedBy("Nikesh");
+		newUser.setAddedTime(date.toString());
+		newUser.setUpdatedBy("NikeshUPD");
+		newUser.setUpdatedTime(date.toString());
+
+		users.add(newUser);
+		// userRepository.save(newUser);
+
+	}
+	
+	public List<User> getAllUser (){
+		return users;
+	}
+
+	public void addNewUser(User user) {
+		System.out.println("inside Save Service");
+		newUser.setFirstName(user.getFirstName());
+		newUser.setLastName(user.getLastName());
+		newUser.setEmail(user.getEmail());
+		newUser.setPassword(user.getPassword());
+		newUser.setEnabled(true);
+		newUser.setAddedBy("Nikesh");
+		newUser.setAddedTime(date.toString());
+		newUser.setUpdatedBy("NikeshUPD");
+		newUser.setUpdatedTime(date.toString());
+		
+		users.add(newUser);
+		
+	}
 
 }
