964ccbdd2e511325f07471cb7d58ced6f0fdb0a2
Nikesh Maharjan <nickesh.maharjan@gmail.com>
2017-08-17 00:37:27 -0400

    authentication

--- src/main/java/com/nick/project/security/AuthorizationServerConfiguration.java
+++ src/main/java/com/nick/project/security/AuthorizationServerConfiguration.java
@@ -4,11 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.oauth2.config.annotation.configuration.ClientDetailsServiceConfiguration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerSecurityConfiguration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
@@ -19,35 +16,35 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
-	private static String REALM = "MY_OAUTH_REALM";
+    private static String REALM = "MY_OAUTH_REALM";
 
-	@Autowired
-	private TokenStore tokenStore;
+    @Autowired
+    private TokenStore tokenStore;
 
-	@Autowired
-	private UserApprovalHandler userApprovalHandler;
+    @Autowired
+    private UserApprovalHandler userApprovalHandler;
 
-	@Autowired
-	@Qualifier("authenticationManagerBean")
-	private AuthenticationManager authenticationManager;
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
 
-	@Override
-	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("my-trusted-client")
-				.authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
-				.authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT").scopes("read", "write", "trust").secret("secret")
-				.accessTokenValiditySeconds(120).refreshTokenValiditySeconds(600);
-	}
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("my-trusted-client")
+                .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT").scopes("read", "write", "trust").secret("secret")
+                .accessTokenValiditySeconds(120).refreshTokenValiditySeconds(600);
+    }
 
-	@Override
-	public void configure(AuthorizationServerEndpointsConfigurer endpointsConfigurer) throws Exception {
-		endpointsConfigurer.tokenStore(tokenStore).userApprovalHandler(userApprovalHandler)
-				.authenticationManager(authenticationManager);
-	}
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpointsConfigurer) throws Exception {
+        endpointsConfigurer.tokenStore(tokenStore).userApprovalHandler(userApprovalHandler)
+                .authenticationManager(authenticationManager);
+    }
 
-	@Override
-	public void configure(AuthorizationServerSecurityConfigurer serverSecurityConfigurer) throws Exception {
-		serverSecurityConfigurer.realm(REALM + "/client");
-	}
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer serverSecurityConfigurer) throws Exception {
+        serverSecurityConfigurer.realm(REALM + "/client");
+    }
 
 }
